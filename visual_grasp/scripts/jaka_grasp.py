bb67a214d0f4b9ef9aa88067efbc8276a1e56b47
HenryWJL <12112921@mail.sustech.edu.cn>
2023-04-25 02:30:58 -0700

    update

--- visual_grasp/scripts/jaka_grasp.py
+++ visual_grasp/scripts/jaka_grasp.py
@@ -17,10 +17,11 @@ def init():
     robot.login()
     robot.power_on()
     robot.enable_robot()
+    robot.set_collision_level(1)
     print("The robotic arm is ready to move")
     ret = robot.get_tcp_position()
     if ret[0] == 0:
-        start_pose = list(ret[1])
+        start_pose = ret[1]
 
     else:
         print("Failed to get the original pose!")
@@ -33,27 +34,31 @@ def grasp_and_place():
         # grasp the object
 
         print("Successful grasp!")
+        time.sleep(3)
+        ret = robot.linear_move(place_pose, 0, True, 1)
+        if ret[0] == 0:
+
+            # place the object
+
+            print("Successful place!")
+            time.sleep(3)
+            ret = robot.linear_move(start_pose, 0, True, 1)
+            if ret[0] == 0:
+                print("Back to the original position!")
+                time.sleep(3)
+                return True
+
+            else:
+                print("Failed to go back to the original position!")
+                return False
+
+        else:
+            print("Failed to move to the place position!")
+            return False
 
     else:
         print("Failed to move to the target position!")
-    time.sleep(3)
-    ret = robot.linear_move(place_pose, 0, True, 1)
-    if ret[0] == 0:
-
-        # place the object
-
-        print("Successful place!")
-
-    else:
-        print("Failed to move to the place position!")
-    time.sleep(3)
-    ret = robot.linear_move(start_pose, 0, True, 1)
-    if ret[0] == 0:
-        print("Back to the original position!")
-
-    else:
-        print("Failed to go back to the original position!")
-    time.sleep(3)
+        return False
 
 
 def callback(pose):
@@ -74,7 +79,15 @@ if __name__ == '__main__':
                     print("No target position is available!")
 
                 else:
-                    grasp_and_place()
+                    success = grasp_and_place()
+                    if not success:
+                        res = robot.is_in_collision()
+                        collision_state = res[1]
+                        if collision_state == 1:
+                            time.sleep(3)
+                            robot.collision_recover()
+                            print("A collision happened, please restart the program!")
+                            break
 
             elif command == 'e':
                 robot.disable_robot()
