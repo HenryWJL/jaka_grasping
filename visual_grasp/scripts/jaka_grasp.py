e511e22be819f4182a809d7cc9eb516608d542c9
HenryWJL <12112921@mail.sustech.edu.cn>
2023-04-29 09:37:48 -0400

    update

--- visual_grasp/scripts/jaka_grasp.py
+++ visual_grasp/scripts/jaka_grasp.py
@@ -9,94 +9,98 @@ import time
 robot = jkrc.RC("192.168.200.100")
 start_pose = []
 target_pose = []
-place_pose = []  # the position where robotic arm places the object
+place_pose = [-0.2664363286039594, -0.015709620237819116, 0.3284071097998305,
+              0.5166579082169311, 1.6366877768864314, -0.5233736902150538]
+# the position where robotic arm places the object
 
 
 def init():
     global start_pose
     robot.login()
-    robot.power_on()
     robot.enable_robot()
     robot.set_collision_level(1)
-    print("The robotic arm is ready to move")
+    rospy.loginfo("The robotic arm is ready to move")
     ret = robot.get_tcp_position()
     if ret[0] == 0:
         start_pose = ret[1]
 
     else:
-        print("Failed to get the original pose!")
+        rospy.logwarn("Failed to get the original pose!")
 
 
 def grasp_and_place():
-    ret = robot.linear_move(target_pose, 0, True, 1)
+    ret = robot.linear_move(target_pose, 0, True, 5)
     if ret[0] == 0:
 
         # grasp the object
 
         print("Successful grasp!")
         time.sleep(3)
-        ret = robot.linear_move(place_pose, 0, True, 1)
+        ret = robot.linear_move(place_pose, 0, True, 5)
         if ret[0] == 0:
 
             # place the object
 
             print("Successful place!")
             time.sleep(3)
-            ret = robot.linear_move(start_pose, 0, True, 1)
+            ret = robot.linear_move(start_pose, 0, True, 5)
             if ret[0] == 0:
-                print("Back to the original position!")
+                rospy.loginfo("Back to the original position!")
                 time.sleep(3)
                 return True
 
             else:
-                print("Failed to go back to the original position!")
+                rospy.logwarn("Failed to go back to the original position!")
                 return False
 
         else:
-            print("Failed to move to the place position!")
+            rospy.logwarn("Failed to move to the place position!")
             return False
 
     else:
-        print("Failed to move to the target position!")
+        rospy.logwarn("Failed to move to the target position!")
         return False
 
 
 def callback(pose):
     global target_pose
-    target_pose = [pose.twist.linear.x, pose.twist.linear.y, pose.twist.linear.z,
+    target_pose = [pose.twist.linear.x, pose.twist.linear.y, pose.twist.linear.z+0.05,
                    pose.twist.angular.x, pose.twist.angular.y, pose.twist.angular.z]
 
 
 if __name__ == '__main__':
     rospy.init_node('jaka_grasp', anonymous=True)
+    rospy.Subscriber('/object_pose', TwistStamped, callback, queue_size=10)
     init()
     while not rospy.is_shutdown():
         try:
-            rospy.Subscriber('/object_pose', TwistStamped, callback, queue_size=10)
-            command = str(input("Grasp: g, E: e"))
+            print("Grasp: g, Exit: e")
+            command = str(input())
             if command == 'g':
+                print("yes")
                 if not target_pose:
-                    print("No target position is available!")
+                    rospy.logwarn("No target position is available!")
 
                 else:
+                    print("yes")
                     success = grasp_and_place()
+                    print("yes")
                     if not success:
                         res = robot.is_in_collision()
                         collision_state = res[1]
                         if collision_state == 1:
                             time.sleep(3)
                             robot.collision_recover()
-                            print("A collision happened, please restart the program!")
+                            rospy.logerr("A collision happened, please restart the program!")
                             break
 
             elif command == 'e':
                 robot.disable_robot()
-                robot.power_off()
                 robot.logout()
                 break
 
             else:
-                print("Invalid option!")
-                    
+                rospy.logwarn("Invalid option!")
+
         except rospy.ROSInterruptException:
-            print("Failed!")
+            rospy.logwarn("Failed!")
--- visual_grasp/scripts/payload_identify.py
+++ visual_grasp/scripts/payload_identify.py
@@ -2,41 +2,53 @@ import jkrc
 import time
 
 PI = 3.14159265
-
+start_pose = [-0.2664363286039594, -0.015709620237819116, 0.3284071097998305,
+              0.5166579082169311, 1.6366877768864314, -0.5233736902150538]
+# joint_pose=[PI/2,PI/3,0,PI/4,0,0]
 robot = jkrc.RC("192.168.200.100")
 robot.login()
 robot.power_on()
 robot.enable_robot()
-robot.set_torsenosr_brand(2)
-robot.set_torque_sensor_mode(1)
-robot.set_compliant_type(1, 1)
-print("inint sensor comple")
-print("ready to run")
-ret = robot.get_joint_position()
-joint_pos_origin = ret[1]
-joint_pos = ret[1]
-print(joint_pos)
-joint_pos[3] += PI / 4
-if joint_pos[3] > 265 * PI / 180:
-    joint_pos[3] -= PI / 2
-    joint_pos[4] += PI / 4
-if joint_pos[4] > 320 * PI / 180:
-    joint_pos[4] -= PI / 2
-    joint_pos[5] += PI / 4
-if joint_pos[5] > 265 * PI / 180:
-    joint_pos[5] -= PI
-print(joint_pos)
-ret = robot.start_torq_sensor_payload_identify(joint_pos)
-time.sleep(1)
-flag = 1
-while 1 == flag:
-    ret = robot.get_torq_sensor_identify_staus()
-    print(ret)
-    time.sleep(1)
-    flag = ret[1]
-print("identy_finish")
-ret = robot.get_torq_sensor_payload_identify_result()
-print(ret)
-robot.joint_move(joint_pos_origin, 0, 1, 10)
-print("back")
+robot.collision_recover()
+ret = robot.get_tcp_position()
+# if ret[0] == 0:
+#     start_pose = ret[1]
+#     print(start_pose)
+# target_pose = [start_pose[0], start_pose[1], start_pose[2]-0.05, start_pose[3], start_pose[4], start_pose[5]]
+target_pose = [0, 0, -10, 0, 0, 0]
+# robot.joint_move(joint_pose, 0, True, 1)
+robot.linear_move(target_pose, 1, True, 5)
+time.sleep(3)
+# robot.set_torsenosr_brand(2)
+# robot.set_torque_sensor_mode(1)
+# robot.set_compliant_type(1, 1)
+# print("inint sensor comple")
+# print("ready to run")
+# ret = robot.get_joint_position()
+# joint_pos_origin = ret[1]
+# joint_pos = ret[1]
+# print(joint_pos)
+# joint_pos[3] += PI / 4
+# if joint_pos[3] > 265 * PI / 180:
+#     joint_pos[3] -= PI / 2
+#     joint_pos[4] += PI / 4
+# if joint_pos[4] > 320 * PI / 180:
+#     joint_pos[4] -= PI / 2
+#     joint_pos[5] += PI / 4
+# if joint_pos[5] > 265 * PI / 180:
+#     joint_pos[5] -= PI
+# print(joint_pos)
+# ret = robot.start_torq_sensor_payload_identify(joint_pos)
+# time.sleep(1)
+# flag = 1
+# while 1 == flag:
+#     ret = robot.get_torq_sensor_identify_staus()
+#     print(ret)
+#     time.sleep(1)
+#     flag = ret[1]
+# print("identy_finish")
+# ret = robot.get_torq_sensor_payload_identify_result()
+# print(ret)
+# robot.joint_move(joint_pos_origin, 0, 1, 10)
+# print("back")
 robot.logout()  # 登出
