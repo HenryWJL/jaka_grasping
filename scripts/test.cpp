a1fd14a5048addd7e79d609a893bc80916b6f2f5
Junlin Wang <12112921@mail.sustech.edu.cn>
2024-01-02 20:51:27 +0800

    Update test.cpp

--- scripts/test.cpp
+++ scripts/test.cpp
@@ -1,4 +1,4 @@
-bool panel_control_callback(sensor_msgs::JointState &joint_states)
+void panel_control_callback(const sensor_msgs::JointState& joint_states)
 {   
     JointValue joint_pose;
     joint_pose.jVal[0] = joint_states.position[0];
@@ -12,16 +12,6 @@ bool panel_control_callback(sensor_msgs::JointState &joint_states)
 
     int sdk_res = robot.joint_move(&joint_pose, MoveMode::ABS, is_block, 0.5, 0.5, 0.2, p);
 
-    switch(sdk_res)
-    {
-        case 0:
-            res.ret = 1;
-            res.message = "cmd has been executed!";
-            break;
-        default:
-            res.ret = sdk_res;
-            res.message = mapErr[sdk_res]+"\n";
-            break;
-    }
-    return true;
 }
+
+ros::Subscriber sub = n.subscribe("jog_panel_command", 10, panel_control_callback);
